4ba3e0aeb4de244425468fdebfcab96fc6af9381
CasparBosch <1004288@hr.nl>
2022-10-13 15:05:23 +0200

    commit

---
+++ example-app/app/Http/Controllers/DeleteController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\delete;
+use Illuminate\Http\Request;
+
+class DeleteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\delete  $delete
+     * @return \Illuminate\Http\Response
+     */
+    public function show(delete $delete)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\delete  $delete
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(delete $delete)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\delete  $delete
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, delete $delete)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\delete  $delete
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(delete $delete)
+    {
+        //
+    }
+}
